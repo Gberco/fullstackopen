05f55feefb11700a56bfa0a71db3d3ee858bdc3f
Gberco <Gberco@gmail.com>
2023-01-24 14:26:09 -0300

    Update newnote04.js

--- part0/newnote04.js
+++ part0/newnote04.js
@@ -1,8 +1,49 @@
-sequenceDiagram
   participant browser
   participant server
   
-  browser->>server: GET https://something
+  browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/notes
   activate server
-  server-->>browser: HTML document
+  server->>browser: HTML document
   deactivate server
+  
+  browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/main.css
+  activate server
+  server->> browser: the css file
+  deactivate server
+  
+  browser ->> server: GET https://studies.cs.helsinki.fi/exampleapp/main.js
+  activate server
+  server ->> browser: the javaScript file
+  deactivate server
+  
+      Note right of browser: The browser starts executing the JavaScript code that fetches the JSON from the server
+  
+  browser ->> server: GET https://studies.cs.helsinki.fi/exampleapp/data.json
+  activate server
+  server ->> browser: [{content: 'jep', date: '2023-01-24T13:02:29.563Z'}, ...]
+  deactivate server
+      Note right of browser: The browser executes the callback function that renders the notes 
+      
+  browser ->> server: POST https://studies.cs.helsinki.fi/exampleapp/new_note
+  activate server
+  server ->> browser: GET /exampleapp/notes
+    Note right of browser: The browser will send the user input to the server and reload Notes page
+  deactivate server
+  
+   browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/main.css
+  activate server
+  server->> browser: the css file
+  deactivate server
+  
+  browser ->> server: GET https://studies.cs.helsinki.fi/exampleapp/main.js
+  activate server
+  server ->> browser: the javaScript file
+  deactivate server
+  
+      Note right of browser: The browser starts executing the JavaScript code that fetches the JSON from the server
+  
+  browser ->> server: GET https://studies.cs.helsinki.fi/exampleapp/data.json
+  activate server
+  server ->> browser: [{newContent: 'something', date: '2023-01-24T14:02:29.563Z'}, ...]
+  deactivate server
+      Note right of browser: The browser executes the callback function that renders the notes 
