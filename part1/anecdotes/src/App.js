97f82390d44a41af2dccd6bab9430f4d03362df9
Gberco <Gberco@gmail.com>
2023-02-15 14:30:52 -0300

    Add files via upload

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+
+const Button = ({action, text}) => {
+  return(
+    <button onClick={ action }>{text}</button>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0})
+
+  const handleSelection = () => {
+    const newAnecdote = Math.floor(Math.random() * anecdotes.length)
+    setSelected(newAnecdote)
+  }
+
+  const handleVote = (selected) => () => {
+    
+    const newPoints = {
+      ...points,
+      [selected]: points[selected] += 1
+    }
+
+    setPoints(newPoints)
+  }
+
+  const mostVotes = () => {
+    const [key] = Object.entries(points)
+      .reduce((r, e) => e[1] > r[1] ? e : r )
+      return key
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {points[selected]} votes </p>
+      <br />
+      <Button action={handleVote(selected)} text='Vote'></Button>
+      <Button action={handleSelection} text='Next anecdote' />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVotes()]}
+    </div>
+  )
+}
+
+
+export default App
